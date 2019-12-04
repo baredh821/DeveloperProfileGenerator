90460ec1ac846849892c42ab92fe3acf48cd5760
baredh821 <baredh821@gmail.com>
2019-12-04 00:01:27 -0600

    Files Updated

--- Develop/lib/htmlRender.js
+++ Develop/lib/htmlRender.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const fs = require("fs"); 
+const fs = require("fs");
 
 const templatesDir = path.resolve(__dirname, "../templates");
 const render = employees => {
@@ -8,12 +8,20 @@ const render = employees => {
         .filter(employee => employee.getRole() === "Manager")
         .map(manager => renderManager(manager))
     )
-
+    html.push(employees
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => renderEngineer(engineer))
+    )
+    html.push(employees
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => renderIntern(intern))
+    )
     return renderMain(html.join(""));
 }
 
+
 const renderManager = manager => {
-    let temp = fs.readFileSync(path.resolve(templatesDir,"manager.html"), "utf8");
+    let temp = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
     temp = replacePlaceholders(temp, "name", manager.getName());
     temp = replacePlaceholders(temp, "role", manager.getRole());
     temp = replacePlaceholders(temp, "email", manager.getEmail());
@@ -24,13 +32,37 @@ const renderManager = manager => {
 }
 
 const renderMain = html => {
-    const temp = fs.readFileSync(path.resolve(templatesDir,"mainhtmlfile.html"), "utf8");
+    const temp = fs.readFileSync(path.resolve(templatesDir, "mainhtmlfile.html"), "utf8");
     return replacePlaceholders(temp, "team", html)
 }
 const replacePlaceholders = (temp, placeholder, value) => {
     const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
     return temp.replace(pattern, value);
-  };
+}
+
+const renderEngineer = engineer => {
+    let temp1 = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+    temp1 = replacePlaceholders(temp1, "name", engineer.getName());
+    temp1 = replacePlaceholders(temp1, "role", engineer.getRole());
+    temp1 = replacePlaceholders(temp1, "email", engineer.getEmail());
+    temp1 = replacePlaceholders(temp1, "id", engineer.getId());
+    temp1 = replacePlaceholders(temp1, "gitHub", engineer.getGitHub());
+    console.log(temp1)
+    return temp1;
+}
+const renderIntern = intern => {
+    let temp1 = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+    temp1 = replacePlaceholders(temp1, "name", intern.getName());
+    temp1 = replacePlaceholders(temp1, "role", intern.getRole());
+    temp1 = replacePlaceholders(temp1, "email", intern.getEmail());
+    temp1 = replacePlaceholders(temp1, "id", intern.getId());
+    temp1 = replacePlaceholders(temp1, "school", intern.getSchool());
+    console.log(temp1)
+    return temp1;
+}
+
+
 
 module.exports = render;
+
 //<h2>Manager name: {{name}}<h2>
